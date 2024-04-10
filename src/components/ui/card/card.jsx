687cc56427a1978f95fae79ee92ef325086ac934
nik1224 <0lezgin5@gmail.com>
2024-04-10 18:51:10 +0500

    Завершает главную страницу

--- src/components/ui/card/card.jsx
+++ src/components/ui/card/card.jsx
@@ -1,21 +1,49 @@
 import React from "react";
+import Tags from "../tags/tags";
 
+export const FarmsTagsCard = {
+  FARMS: "Фермерские продукты",
+  SHOPS: "Магазинные продукты"
+}
 
 function Card({ tag, name, image, desc }) {
-  return (
+  let options;
+
+  switch (tag) {
+    case FarmsTagsCard.FARMS:
+      options = {
+        text: "Фермерские продукты",
+        bgColor: "#E1EDCE"
+      };
+      break;
+    case FarmsTagsCard.SHOPS:
+      options = {
+        text: "Магазинные продукты",
+        bgColor: "#F8DDD7"
+      };
+      break;
+    default:
+      options = {
+        text: "",
+        bgColor: "#000000"
+      };
+      break;
+  }
+
+  return options.text ? (
     <React.Fragment>
-      <article className="advantages__item advantages__item--farms">
+      <article className="advantages__item" style={{ backgroundColor: options.bgColor }}>
         <div className="advantages__wrapper-start">
           <img src={image} alt="" />
           <div>
-            <span className="advantages__tag">{tag}</span>
+            <Tags tag={tag} />
             <h3 className="advantages__name">{name}</h3>
           </div>
         </div>
         <p dangerouslySetInnerHTML={{ __html: desc }} />
       </article>
     </React.Fragment>
-  );
+  ) : null;
 }
 
 export default Card;
